177e3a6dd4b5677d44410282cf02e36d668e5353
drewbolles <drewbolles@gmail.com>
2014-05-21 21:48:35 -0400

    add hook css to unset system css files

--- template.php
+++ template.php
@@ -7,3 +7,18 @@ function gotham_preprocess_page(&$vars){
 function gotham_preprocess_html(&$vars){
 
 }
+
+function gotham_css_alter(&$css){
+  $exclude = array(
+    'modules/system/system.css' => FALSE,
+    'modules/system/system.admin.css' => FALSE,
+    'modules/system/system.base.css' => FALSE,
+    'modules/system/system.maintenance.css' => FALSE,
+    'modules/system/system.menus.css' => FALSE,
+    'modules/system/system.messages.css' => FALSE,
+    'modules/system/system.theme.css' => FALSE,
+  );
+  
+  $css = array_diff_key($css, $exclude);
+  
+}
