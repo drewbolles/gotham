6eb1c387a18475a0a3e726c833fc0a1b8a726f8c
drewbolles <drewbolles@gmail.com>
2014-12-02 14:24:59 -0800

    comment scripts file better

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,18 @@
 (function($){
+	/* when document is ready */
 	$(document).ready(function(){
 
 	});
+
+	/* when window is ready */
 	$(window).ready(function(){
 		
 	});
+
+	/* theme Drupal behaviors */
+	Drupal.behaviors.Gotham = {
+		attach: function(context, settings) {
+
+		}
+	}
 })(jQuery);
