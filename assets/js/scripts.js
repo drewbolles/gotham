1db26086adcbf0d36773ff443b831ade5c7c8a44
drewbolles <drewbolles@gmail.com>
2015-05-04 14:51:21 -0700

    add mobile menu toggle script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,11 @@
 (function($){
+	// console.log('works');
 	/* when document is ready */
 	$(document).ready(function(){
-
+		$('#menu-toggle').click(function(e){
+			$('body').toggleClass('menu-open');
+			e.preventDefault();
+		});
 	});
 
 	/* when window is ready */
