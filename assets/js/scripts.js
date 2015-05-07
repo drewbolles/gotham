7c874f39eac6655037b035c46715703ec5b35693
drewbolles <drewbolles@gmail.com>
2015-05-07 10:18:38 -0700

    add FFO script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,22 @@
+/**
+ * Font Face Observer
+ */
+var roboto = new FontFaceObserver( "Roboto" );
+
+Promise.([
+	roboto.check()
+]).then(function() {
+	document.documentElement.className += " fonts-loaded";
+}); 
+
+/**
+ * jQuery script
+ */
 (function($){
 	// console.log('works');
 	/* when document is ready */
 	$(document).ready(function(){
+
 		$('#menu-toggle').click(function(e){
 			$('body').toggleClass('menu-open');
 			e.preventDefault();
