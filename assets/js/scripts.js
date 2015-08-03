eb7c10992e38252879d65ccc83c09bddc2e8a3be
drewbolles <drewbolles@gmail.com>
2015-08-03 14:56:21 -0700

    move js into drupal behavior

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,26 +13,13 @@ Promise.all([
  * jQuery script
  */
 (function($){
-	// console.log('works');
-	/* when document is ready */
-	$(document).ready(function(){
-
-		$('#menu-toggle').bind( "tap", function( e ) {
-			$('body').toggleClass('menu-open');
-			e.preventDefault();
-		});
-
-	});
-
-	/* when window is ready */
-	$(window).ready(function(){
-		
-	});
-
-	/* theme Drupal behaviors */
-	Drupal.behaviors.Gotham = {
+	Drupal.behaviors.gotham = {
 		attach: function(context, settings) {
-
+			// console.log('works');
+			$('#menu-toggle', context).bind( "tap", function( e ) {
+				$('body', context).toggleClass('menu-open');
+				e.preventDefault();
+			});
 		}
 	}
 })(jQuery);
