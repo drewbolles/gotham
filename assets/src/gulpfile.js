80d0a9a462c391df35b31fb6d75b074146bcd502
Drew Bolles <drewbolles@gmail.com>
2016-05-26 14:15:53 -0700

    change sass output to compressed, use es6 syntax in gulpfile.js

--- assets/src/gulpfile.js
+++ assets/src/gulpfile.js
@@ -2,37 +2,37 @@
  * @file
  * Gulp script to run build process.
  */
-'use strict';
 
 // Require Gulp
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var prefix = require('gulp-autoprefixer');
-var watch = require('gulp-watch');
-var plumber = require('gulp-plumber');
-var babel = require('gulp-babel');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const prefix = require('gulp-autoprefixer');
+const watch = require('gulp-watch');
+const plumber = require('gulp-plumber');
+const babel = require('gulp-babel');
 
 // Gulp Sass Task
-gulp.task('sass', function() {
+gulp.task('sass', () => {
   gulp.src('sass/**/*.scss')
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(prefix('last 2 versions', '> 1%', 'ie 8'))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('../css'));
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', () => {
   return gulp
     .src('js/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('../js'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
   gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('js/**/*.js', ['scripts']);
 });
 
 gulp.task('default', ['sass', 'scripts', 'watch']);
